6c1449a85165bdb117fa5efc8304f6bdfe98ea26
Giorgos Kontopoulos <kongeo@gmail.com>
2025-12-27 14:19:59 +0200

    Make certain checkbox fields to behave like radio buttons

---
+++ web/modules/custom/checkbox_radios/js/checkbox_radios.js
@@ -0,0 +1,50 @@
+(function (Drupal, once, drupalSettings) {
+  'use strict';
+
+  const toDrupalSelector = (fieldName) =>
+    `edit-${fieldName.replace(/_/g, '-')}`;
+
+  const findFieldWrappers = (context, fieldName) => {
+    const selector = `[data-drupal-selector="${toDrupalSelector(fieldName)}"]`;
+    return Array.from(context.querySelectorAll(selector));
+  };
+
+  const bindCheckboxGroup = (wrapper) => {
+    const checkboxes = Array.from(
+      wrapper.querySelectorAll('input[type="checkbox"]')
+    );
+
+    if (checkboxes.length === 0) {
+      return;
+    }
+
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', () => {
+        if (!checkbox.checked) {
+          return;
+        }
+
+        checkboxes.forEach((other) => {
+          if (other !== checkbox) {
+            other.checked = false;
+          }
+        });
+      });
+    });
+  };
+
+  Drupal.behaviors.checkboxRadios = {
+    attach(context) {
+      const fields = drupalSettings.checkboxRadios?.fields || [];
+      if (fields.length === 0) {
+        return;
+      }
+
+      fields.forEach((fieldName) => {
+        findFieldWrappers(context, fieldName).forEach((wrapper) => {
+          once('checkbox-radios', wrapper).forEach(bindCheckboxGroup);
+        });
+      });
+    },
+  };
+})(Drupal, once, drupalSettings);
